bc9bab07715d7161418f9834e2cd6aa5e3b22dbc
mz403079 <mz403079@users.noreply.github.com>
2020-12-15 18:18:09 +0100

    Zmiana nazw metod w interfejsie RhymersFactory.

--- src/main/java/edu/kis/vh/nursery/factory/RhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/RhymersFactory.java
@@ -2,8 +2,9 @@ package edu.kis.vh.nursery.factory;
 
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 	//Renamed Interface to RhymersFactory.
+	//Renamed methods.
 	public interface RhymersFactory {
-	
+
 		public DefaultCountingOutRhymer getStandardRhymer();
 		
 		public DefaultCountingOutRhymer getFalseRhymer();
